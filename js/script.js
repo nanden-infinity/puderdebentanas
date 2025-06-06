42f49364c1177a4b3b25b984356f517e1ad99661
Inanden <147476553+Inanden@users.noreply.github.com>
2025-06-06 15:17:52 +0100

    altercao sections

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
+const button = document.querySelector('.find-projecto');
 
-export default  class Elemet {
-  constructor(element, text){
-    this.elemento = element;
-    this.texto = text;
-  }
-    create(){
-      const texto = 'Ideias do Projecto'
-        this.elemento = document
-    }
-}
-
-const insertElement =  function(){
-  const button = document.createElement('button')
-  const banner = document.querySelector('.figure--banner');
-  button.classList.add('toogles')
-  button.innerText = 'Ideia de Projecto'
-  banner.appendChild(button)
-  console.log(button);
-}
-
-insertElement()
+button.addEventListener('click', () =>{
+   alert('HELLLO WORLD')
+})
