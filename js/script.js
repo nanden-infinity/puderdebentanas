93d2ca345dbbcadcf675c8c5fbc00827e0e418d4
Inanden <147476553+Inanden@users.noreply.github.com>
2025-06-23 15:43:53 +0100

    section proximos passos add

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const header  = new HeaderElement('.container', 'header')
 const modalContainer = document.querySelector('.modal');
 
 
-const openModal = () =>{º
+const openModal = () =>{
    console.log('Hi Hello');
 }
 
